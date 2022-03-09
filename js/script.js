29e1d07dd48d9974daa30ea5bc407f7f23474248
John <john.njoroge@student.moringaschool.com>
2022-03-09 15:57:17 +0300

    Added user input section

--- js/script.js
+++ js/script.js
@@ -5,32 +5,32 @@
 let Calculator = document.getElementById('calc');
 
 var add = (num1, num2) =>{
-    var num1 = parseInt(prompt("Enter the first number"));
-    var num2 = parseInt(prompt("Enter the second number"));
+    // var num1 = parseInt(prompt("Enter the first number"));
+    // var num2 = parseInt(prompt("Enter the second number"));
     return num1 + num2;
 };
-alert(add());
+// alert(add());
 
 var subtract = (num1, num2) =>{
-    var num1 = parseInt(prompt("Enter the first number"));
-    var num2 = parseInt(prompt("Enter the second number"));
+//     var num1 = parseInt(prompt("Enter the first number"));
+//     var num2 = parseInt(prompt("Enter the second number"));
     return num1 - num2;
 };
-alert(subtract());
+// alert(subtract());
 
 var multiply = (num1, num2) =>{
-    var num1 = parseInt(prompt("Enter the first number"));
-    var num2 = parseInt(prompt("Enter the second number"));
+//     var num1 = parseInt(prompt("Enter the first number"));
+//     var num2 = parseInt(prompt("Enter the second number"));
     return num1 * num2;
 };
-alert(multiply());
+// alert(multiply());
 
 var divide = (num1, num2) =>{
-    var num1 = parseInt(prompt("Enter the first number"));
-    var num2 = parseInt(prompt("Enter the second number"));
+    // var num1 = parseInt(prompt("Enter the first number"));
+    // var num2 = parseInt(prompt("Enter the second number"));
     return num1 / num2;
 };
-alert(divide());
+// alert(divide());
 
 
 
