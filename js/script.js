5487002410df3da3b3c363170747ba2be1852812
John <john.njoroge@student.moringaschool.com>
2022-03-11 11:25:50 +0300

    feat: bmi calculation section

--- js/script.js
+++ js/script.js
@@ -2,40 +2,52 @@
 // parsing ints
 // simple prompt claculator 
 
-let Calculator = document.getElementById('calc');
-
-var add = (num1, num2) =>{
-    // var num1 = parseInt(prompt("Enter the first number"));
-    // var num2 = parseInt(prompt("Enter the second number"));
-    return num1 + num2;
-};
-// alert(add());
-
-var subtract = (num1, num2) =>{
-//     var num1 = parseInt(prompt("Enter the first number"));
-//     var num2 = parseInt(prompt("Enter the second number"));
-    return num1 - num2;
-};
-// alert(subtract());
-
-var multiply = (num1, num2) =>{
-//     var num1 = parseInt(prompt("Enter the first number"));
-//     var num2 = parseInt(prompt("Enter the second number"));
-    return num1 * num2;
-};
-// alert(multiply());
-
-var divide = (num1, num2) =>{
-    // var num1 = parseInt(prompt("Enter the first number"));
-    // var num2 = parseInt(prompt("Enter the second number"));
-    return num1 / num2;
-};
-// alert(divide());
 
 
+document.getElementById('add').addEventListener('click', function(){
+    var firstNumber = parseInt(document.getElementById('firstNo').value);
+    var secondNumber = parseInt(document.getElementById('secondNo').value);
+    let calculator = document.getElementById('calc');
 
-var bodyMassIndex = (weight, height) => {
-    var height = parseInt(prompt("Enter your weight(kgs)"));
-    var height = parseInt(prompt("Enter your height(kgs)"));
-    return weight / height;
-};
+    calculator.innerHTML = firstNumber + secondNumber;
+});
+
+document.getElementById('subtract').addEventListener('click', function(){
+    var firstNumber = parseInt(document.getElementById('firstNo').value);
+    var secondNumber = parseInt(document.getElementById('secondNo').value);
+    let calculator = document.getElementById('calc');
+
+    calculator.innerHTML = firstNumber - secondNumber;
+});
+
+document.getElementById('multiply').addEventListener('click', function(){
+    var firstNumber = parseInt(document.getElementById('firstNo').value);
+    var secondNumber = parseInt(document.getElementById('secondNo').value);
+    let calculator = document.getElementById('calc');
+
+    calculator.innerHTML = firstNumber * secondNumber;
+});
+
+document.getElementById('divide').addEventListener('click', function(){
+    var firstNumber = parseInt(document.getElementById('firstNo').value);
+    var secondNumber = parseInt(document.getElementById('secondNo').value);
+    let calculator = document.getElementById('calc');
+
+    calculator.innerHTML = firstNumber / secondNumber;
+});
+
+document.getElementById('bmi').addEventListener('click', function(){
+    var height = parseFloat(document.getElementById('height').value);
+    var weight = parseFloat(document.getElementById('weight').value);
+    let calculator = document.getElementById('calc');
+
+    var bmi = weight / height;
+    calculator.innerHTML = bmi;
+
+    if (bmi > 25){
+        alert(`Your BMI is ${bmi}. You are in danger`)
+    }
+
+});
+
+// console.log( 2 / 4);html
